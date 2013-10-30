c64f73e2eb89545a6e52a4b2d0d559cb4a0d542d
Maxternal <maxternal@gmail.com>
2013-10-30 13:45:28 -0600

    reverting back to the less glitchy way of sending the screen out put one character at a time since there's really no speed difference for what we're doing.

--- gecko.c
+++ gecko.c
@@ -242,22 +242,31 @@ u8 gecko_enable_console(const u8 enable)
 	return gecko_console_enabled;
 }
 
+void print_to_screen(const char*fmt)
+{
+	while(*fmt)
+	{	do dc_invalidaterange((void*)0x01200000,32);
+		while(read8(0x01200000));
+		write8(0x01200000, *fmt);
+		dc_flushrange((void*)0x01200000,32);
+		fmt++;
+	}
+}
+
 #ifndef NDEBUG
 int gecko_printf(const char *fmt, ...)
 {	
 	if(!(gecko_enabled & 1))
 		return 0;
 	va_list args;
-	char* buffer = (char*)0x01200008;
+	char buffer[256];
 	int i;
 
 	va_start(args, fmt);
-	do dc_invalidaterange((void*)0x01200000,32);
-	while(read8(0x01200000));
 	i = vsprintf(buffer, fmt, args);
 	va_end(args);
-	write8(0x01200000, 1);
-	dc_flushrange((void*)0x01200000,0x120);
+	fmt = buffer;
+	print_to_screen(fmt);
 	return 0;
 #ifdef GECKO_SAFE
 	return gecko_sendbuffer_safe(buffer, i);
@@ -272,16 +281,14 @@ int screen_printf(const char *fmt, ...)
 	if(!(gecko_enabled & 5))
 		return 0;
 	va_list args;
-	char* buffer = (char*)0x01200008;
+	char buffer[256];
 	int i;
 
 	va_start(args, fmt);
-	do dc_invalidaterange((void*)0x01200000,32);
-	while(read8(0x01200000));
 	i = vsprintf(buffer, fmt, args);
 	va_end(args);
-	write8(0x01200000, 1);
-	dc_flushrange((void*)0x01200000,0x120);
+	fmt = buffer;
+	print_to_screen(fmt);
 	return 0;
 #ifdef GECKO_SAFE
 	return gecko_sendbuffer_safe(buffer, i);
